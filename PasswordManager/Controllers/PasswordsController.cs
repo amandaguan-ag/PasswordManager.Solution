7bf946ad4aba57fc4bece8e50ff7a68fd4a2f2ac
Amanda Guan <ag.amandaguan@gmail.com>
2023-07-02 12:54:50 -0700

    fix home/index.cshtml

--- PasswordManager/Controllers/PasswordsController.cs
+++ PasswordManager/Controllers/PasswordsController.cs
@@ -6,71 +6,71 @@ using System.Linq;
 
 namespace PasswordManager.Controllers
 {
-  public class PasswordsController : Controller
-  {
-    private readonly PasswordManagerContext _db;
-
-    public PasswordsController(PasswordManagerContext db)
+    public class PasswordsController : Controller
     {
-      _db = db;
-    }
+        private readonly PasswordManagerContext _db;
 
-public ActionResult Index()
-{
-    List<Password> model = _db.Passwords.ToList();
-    return View(model);
-}
+        public PasswordsController(PasswordManagerContext db)
+        {
+            _db = db;
+        }
 
-    public ActionResult Create()
-    {
-      return View();
-    }
+        public ActionResult Index()
+        {
+            List<Password> model = _db.Passwords.ToList();
+            return View(model);
+        }
 
-    [HttpPost]
-    public ActionResult Create(Password password)
-    {
-      _db.Passwords.Add(password);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-    public ActionResult Details(int id)
-    {
-      Password thisPassword = _db.Passwords
-                                // .Include(cat => cat.Passwords)
-                                // .ThenInclude(Password => Password.JoinEntities)
-                                // .ThenInclude(join => join.Tag)
-                                .FirstOrDefault(password => password.PasswordId == id);
-      return View(thisPassword);
-    }
+        [HttpPost]
+        public ActionResult Create(Password password)
+        {
+            _db.Passwords.Add(password);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-    public ActionResult Edit(int id)
-    {
-      Password thisPassword = _db.Passwords.FirstOrDefault(password => password.PasswordId == id);
-      return View(thisPassword);
-    }
+        public ActionResult Details(int id)
+        {
+            Password thisPassword = _db.Passwords
+                                      // .Include(password => password.Passwords)
+                                      // .ThenInclude(Password => Password.JoinEntities)
+                                      // .ThenInclude(join => join.Tag)
+                                      .FirstOrDefault(password => password.PasswordId == id);
+            return View(thisPassword);
+        }
 
-    [HttpPost]
-    public ActionResult Edit(Password password)
-    {
-      _db.Passwords.Update(password);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+        public ActionResult Edit(int id)
+        {
+            Password thisPassword = _db.Passwords.FirstOrDefault(password => password.PasswordId == id);
+            return View(thisPassword);
+        }
 
-    public ActionResult Delete(int id)
-    {
-      Password thisPassword = _db.Passwords.FirstOrDefault(password => password.PasswordId == id);
-      return View(thisPassword);
-    }
+        [HttpPost]
+        public ActionResult Edit(Password password)
+        {
+            _db.Passwords.Update(password);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-    [HttpPost, ActionName("Delete")]
-    public ActionResult DeleteConfirmed(int id)
-    {
-      Password thisPassword = _db.Passwords.FirstOrDefault(password => password.PasswordId == id);
-      _db.Passwords.Remove(thisPassword);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
+        public ActionResult Delete(int id)
+        {
+            Password thisPassword = _db.Passwords.FirstOrDefault(password => password.PasswordId == id);
+            return View(thisPassword);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Password thisPassword = _db.Passwords.FirstOrDefault(password => password.PasswordId == id);
+            _db.Passwords.Remove(thisPassword);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-  }
 }
