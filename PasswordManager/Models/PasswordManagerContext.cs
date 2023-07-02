378d7adc90691b5cd0aa96268de9f54c35127ba5
Billy Lee <codingbillylee@gmail.com>
2023-07-02 15:00:32 -0700

    Add delete and edit functions

--- PasswordManager/Models/PasswordManagerContext.cs
+++ PasswordManager/Models/PasswordManagerContext.cs
@@ -8,5 +8,19 @@ namespace PasswordManager.Models
     public DbSet<Password> Passwords { get; set; }
 
     public PasswordManagerContext(DbContextOptions options) : base(options) { }
-  }
+    
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //     {
+    //         base.OnModelCreating(modelBuilder);
+
+    //         modelBuilder.Entity<Password>(entity =>
+    //         {
+    //             entity.Property(e => e.SiteEmail)
+    //                   .IsRequired()
+    //                   .HasMaxLength(256)
+    //                   .HasAnnotation("RegularExpression", "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")
+    //                   .HasAnnotation("MaxLength", 256);
+    //         });
+    //     }
+    }
 }
