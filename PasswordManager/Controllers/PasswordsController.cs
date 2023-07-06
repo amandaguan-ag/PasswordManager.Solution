5140b9d4dd88bbcbc481a43efad33744689579cb
Billy Lee <codingbillylee@gmail.com>
2023-07-05 19:10:05 -0700

    Add final authorization feature.

--- PasswordManager/Controllers/PasswordsController.cs
+++ PasswordManager/Controllers/PasswordsController.cs
@@ -1,11 +1,17 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using PasswordManager.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace PasswordManager.Controllers
 {
+    [Authorize]
     public class PasswordsController : Controller
     {
         private readonly PasswordManagerContext _db;
